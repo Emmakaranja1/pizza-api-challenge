8990cfe678b144ff287c523cb515b7e0a5ff8d8b
Emmakaranja1 <karanjaemmak@gmail.com>
2025-06-20 08:33:01 +0300

    define Pizza model with name, ingredients, and relationship to RestaurantPizza

---
+++ server/models/pizza.py
@@ -0,0 +1,16 @@
+from server.database import db
+
+class Pizza(db.Model):
+    __tablename__ = 'pizzas'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    ingredients = db.Column(db.String(200), nullable=False)
+
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='pizza', cascade='all, delete')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'ingredients': self.ingredients
+        }
